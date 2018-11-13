75de83e9a2a3343f8d2ec40d65f22e67e649e141
Atilla GÜNDÜZ <gunduz.atilla@gmail.com>
2018-11-13 13:50:55 +0300

    update toplevel domain setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # enable app_name.domain
+  config.action_dispatch.tld_length = 0
 end
